9aca2de4879a6ced5a7423abfa455a1dac4806ee
flyicarus-net <ripollh@hotmail.com>
2023-05-03 16:25:31 +0200

    primer.php

---
+++ primer.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primer PHP</title>
+</head>
+<body>
+    <h1>Pagina PHP</h1>
+    <?php
+        //Código de PHP
+        echo "Hola mund00000o";
+    ?>
+</body>
+</html>
